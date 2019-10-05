ce3d6212f83c596dc25e5c4c95e81f8c54307946
gelius <gelius7@gmail.com>
2019-10-05 15:48:24 +0900

    path change
    path change

--- modules/90-local-file.tf
+++ modules/90-local-file.tf
@@ -2,25 +2,25 @@
 
 resource "local_file" "aws_auth" {
   content  = "${data.template_file.aws_auth.rendered}"
-  filename = "/tmp/.output/aws_auth.yaml"
+  filename = "${path.module}/.output/aws_auth.yaml"
 }
 
 resource "local_file" "kube_config" {
   content  = "${data.template_file.kube_config.rendered}"
-  filename = "/tmp/.output/kube_config.yaml"
+  filename = "${path.module}/.output/kube_config.yaml"
 }
 
 resource "local_file" "kube_config_secret" {
   content  = "${data.template_file.kube_config_secret.rendered}"
-  filename = "/tmp/.output/kube_config_secret.yaml"
+  filename = "${path.module}/.output/kube_config_secret.yaml"
 }
 
 resource "local_file" "cluster_role_binding_admin" {
   content  = "${data.template_file.cluster_role_binding_admin.rendered}"
-  filename = "/tmp/.output/cluster_role_binding_admin.yaml"
+  filename = "${path.module}/.output/cluster_role_binding_admin.yaml"
 }
 
 resource "local_file" "cluster_role_binding_view" {
   content  = "${data.template_file.cluster_role_binding_view.rendered}"
-  filename = "/tmp/.output/cluster_role_binding_view.yaml"
+  filename = "${path.module}/.output/cluster_role_binding_view.yaml"
 }
